1d4bba4449118f9e8e224a5b3ab15de4f4d61f60
griimnak <griimnak@gmail.com>
2018-05-16 01:31:47 -0400

    white text if bg has img

--- js/widgets/background.js
+++ js/widgets/background.js
@@ -2,7 +2,7 @@ function setBackground() {
   if (settings.background.type == 'image') {
     document.body.style.background = 'url(' + settings.background.value + ')  no-repeat';
     document.body.style.backgroundSize = '100% 100%';
-    document.getElementById("clocktext").style.color = "#ffffff";
+    document.body.style.color = "#ffffff";
     document.getElementById("settings").style.color = "#ffffff";
     document.getElementById('overlay').innerHTML += '<div class="fullscreenOverlay"></div>';
 
